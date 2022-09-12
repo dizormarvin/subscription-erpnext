7a732142308d1f0e2528745c078c34dd1199f161
jeowsome <jeowsome15@gmail.com>
2022-09-12 12:46:38 +0800

    added ph island and region

---
+++ subscription/subscription/doctype/philippine_islands/philippine_islands.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, ossphin and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Philippine Islands', {
+	// refresh: function(frm) {
+
+	// }
+});
